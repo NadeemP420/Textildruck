6d9a107c9857b10e17935b13def3da308e32f82b
NadeemP420 <nadeem420@hotmail.de>
2025-05-24 04:07:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿document.getElementById("logoInput").addEventListener("change", function(e) {
+    const file = e.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(evt) {
+            document.getElementById("logoPreview").src = evt.target.result;
+            document.getElementById("logoPreview").style.display = "block";
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+
+document.getElementById("customText").addEventListener("input", function() {
+    document.getElementById("textPreview").textContent = this.value;
+});
+
+
+function showFront() {
+    document.getElementById("shirtImage").src = "https://i.imgur.com/IEhV6YQ.png";
+}
+
+
+function showBack() {
+    document.getElementById("shirtImage").src = "https://i.imgur.com/t7eU7k1.png";
+}
